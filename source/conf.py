70a744e0aa8e8835454b41459c818d7a18beeeae
edifighter <edifighter@outlook.com>
2023-12-10 19:48:31 +0800

    add ext for theme

--- source/conf.py
+++ source/conf.py
@@ -48,6 +48,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'python_docs_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
