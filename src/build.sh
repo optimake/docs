b730f78cd970ca2c437354d6bc90413a459cb17e
spark <spark.optimake@outlook.com>
2024-03-07 00:17:12 +0800

    update

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+rm -rf site
+echo "Build Website!"
 python3 -m mkdocs build
+
+echo "Clean Website Code!"
+rm -rf ../docs/*
+mkdir -p ../docs
 echo "Update Website Code!"   
 cp -rf site/* ../docs/
